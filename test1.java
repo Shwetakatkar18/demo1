9ab3261ab89254941c6ba0730dbafe30a4a9b5cd
Shwetakatkar18 <shwetakatkar2003@gmail.com>
2025-04-03 15:20:39 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -6,8 +6,8 @@ public class numbers{
 			System.out.println("Enter two numbers");
 		int num1= sc.nextInt();
 		int num2= sc.nextInt();
-		int add= num1+num2;
-		System.out.println("Addition of two numbers are:");
+		int mul= num1*num2;
+		System.out.println("Multiplication of two numbers are:");
 		sc.close();
 	}
 }
